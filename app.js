48d1b4179bb79a77e115db3f6264e0cfbd0527b6
Harsh Vagh <110323765+HRV1527@users.noreply.github.com>
2023-12-05 21:49:59 +0530

    Add Env variables

---
+++ app.js
@@ -0,0 +1,66 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import bcrypt from "bcrypt";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+    user:"postgres",
+    host:"localhost",
+    database:"Authen",
+    password:"harsh@1527",
+    port:5432
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'))
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs")
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login.ejs")
+});
+
+app.get("/register",(req,res)=>{
+    res.render("register.ejs")
+});
+
+app.post("/register", (req,res)=>{
+    try{
+        bcrypt.hash(req.body.password,10).then(async (hash)=>{
+            await db.query("INSERT INTO users VALUES(DEFAULT,$1,$2)",[req.body.username,hash]);
+            res.redirect("/");
+        })
+        
+    }catch(err){
+        console.error({error: err.message});
+    }
+});
+
+app.post("/login", async (req,res)=>{
+    let username = req.body.username
+    let password = req.body.password
+
+    try{
+        const result = await db.query("SELECT email, user_pass FROM users WHERE email=$1;",[username]);
+        const match = await bcrypt.compare(password,result.rows[0].user_pass);
+        if(match){
+            res.render("secrets.ejs");
+        }else{
+            console.log("Unauthorized.")
+            res.sendStatus(401)
+        }
+    }catch(err){
+        console.error({error: err.message});
+    }
+});
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
